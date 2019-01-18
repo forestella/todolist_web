ebdd17b89d582150d937c632ccd1acb0894ed699
SeougUk <zeldatt@gmail.com>
2019-01-18 14:43:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/starlight/todolist/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.starlight.todolist.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello World";
+    }
+
+}
