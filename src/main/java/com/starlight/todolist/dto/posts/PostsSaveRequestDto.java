49aa9b5050542f344b8ce61429f0f530a6e14e3e
SeougUk <zeldatt@gmail.com>
2019-01-19 11:59:48 +0900

    dto 추가

---
+++ src/main/java/com/starlight/todolist/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.starlight.todolist.dto.posts;
+
+import com.starlight.todolist.web.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
