af1de7b901acfb960fd420e2f43f539f1856b17c
SeougUk <zeldatt@gmail.com>
2019-01-19 10:26:41 +0900

    테스트 코드 작성

--- src/main/java/com/starlight/todolist/web/WebRestController.java
+++ src/main/java/com/starlight/todolist/web/WebRestController.java
@@ -1,14 +1,23 @@
 package com.starlight.todolist.web;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
     @GetMapping("/hello")
     public String Hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 
 }
--- src/test/java/com/starlight/todolist/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/starlight/todolist/web/domain/posts/PostsRepositoryTest.java
@@ -1,4 +1,48 @@
 package com.starlight.todolist.web.domain.posts;
 
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("안녕하세요.")
+                .author("zeldatt@gmail.com")
+                .content("테스트 글 작성")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        assertThat(posts.getTitle(), is("안녕하세요."));
+        assertThat(posts.getContent(), is("테스트 글 작성"));
+        assertThat(posts.getAuthor(), is("zeldatt@gmail.com"));
+
+
+    }
+
 }
